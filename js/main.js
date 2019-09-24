d867a937558dbed3407aced242bd05891919757a
3dvolt <3dvolt.info@gmail.com>
2019-09-24 20:24:01 +0200

    center text

--- js/main.js
+++ js/main.js
@@ -13,3 +13,12 @@ $('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
     next.children(':first-child').clone().appendTo($(this));
   }
 });
+
+(function($) {
+    "use strict";
+
+    // manual carousel controls
+    $('.next').click(function(){ $('.carousel').carousel('next');return false; });
+    $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
+    
+})(jQuery);
